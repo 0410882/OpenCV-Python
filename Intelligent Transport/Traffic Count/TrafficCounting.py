52ac0c1f9221e59126ee3eb4b2f1b3c39f9ddcdc
0410882 <lxn9715@gmail.com>
2020-09-01 21:22:14 +0800

    Add files via upload

--- Intelligent Transport/Traffic Count/TrafficCounting.py
+++ Intelligent Transport/Traffic Count/TrafficCounting.py
@@ -1,23 +1,17 @@
-import matplotlib.pyplot as plt
 import cv2
 import numpy as np
 
 
 class TrafficCounting:
 
-    def show_images(self, images, cmap=None):
-        cols = 2
-        rows = (len(images)+1)//cols
-        
-        plt.figure(figsize=(15, 12))
-        for i, image in enumerate(images):
-            plt.subplot(rows, cols, i+1)
-            cmap = 'gray' if len(image.shape)==2 else cmap
-            plt.imshow(image, cmap=cmap)
-            plt.xticks([])
-            plt.yticks([])
-        plt.tight_layout(pad=0, h_pad=0, w_pad=0)
-        plt.show()
+    #set calculate Lines
+    calculateRange_y = 0
+    calculateLine1 = [[] for i in range(2)]
+    calculateLine2 = [[] for i in range(2)]
+    #Initialize count variables
+    count_Car1 = 0
+    count_Car2 = 0
+
 		
     # show image
     def cv_show(self, name, img):
@@ -37,12 +31,12 @@ class TrafficCounting:
 		"""
         mask1 = np.zeros_like(image)
         mask2 = np.zeros_like(image)
-        if len(mask1.shape) == 2:
+        if len(mask1.shape) == 3:
             cv2.fillPoly(mask1, vertices1, 255)  # 填充mask
-        if len(mask2.shape) == 2:
+        if len(mask2.shape) == 3:
             cv2.fillPoly(mask2, vertices2, 255)  # 填充mask
         mask1 = cv2.bitwise_or(mask1, mask2)
-        return cv2.bitwise_and(image, mask1)
+        return mask1
 
     def select_region(self, image):
         """
@@ -50,16 +44,22 @@ class TrafficCounting:
 		"""
         # first, define the polygon by vertices
         rows, cols = image.shape[:2]
-        pt_1l = [cols * 0.15, rows * 0.45]
-        pt_2l = [cols * 0.15, rows * 0.55]
-        pt_3l = [cols * 0.49, rows * 0.55]
-        pt_4l = [cols * 0.49, rows * 0.45]
+        pt_1l = [cols * 0.15, rows * 0.44]
+        pt_2l = [cols * 0.15, rows * 0.60]
+        pt_3l = [cols * 0.49, rows * 0.60]
+        pt_4l = [cols * 0.49, rows * 0.44]
 		
-        pt_1r = [cols * 0.51, rows * 0.45]
-        pt_2r = [cols * 0.51, rows * 0.55]
-        pt_3r = [cols * 0.85, rows * 0.55]
-        pt_4r = [cols * 0.85, rows * 0.45]
+        pt_1r = [cols * 0.51, rows * 0.44]
+        pt_2r = [cols * 0.51, rows * 0.60]
+        pt_3r = [cols * 0.85, rows * 0.60]
+        pt_4r = [cols * 0.85, rows * 0.44]
 
+        self.calculateLine1[0] = [cols * 0.15, rows * 0.52]
+        self.calculateLine1[1] = [cols * 0.49, rows * 0.52]
+        self.calculateLine2[0] = [cols * 0.51, rows * 0.52]
+        self.calculateLine2[1] = [cols * 0.85, rows * 0.52]
+        self.calculateRange_y = rows * 0.08
+		
         vertices1 = np.array([[pt_1l, pt_2l, pt_3l, pt_4l]], dtype=np.int32)
         vertices2 = np.array([[pt_1r, pt_2r, pt_3r, pt_4r]], dtype=np.int32)
         
